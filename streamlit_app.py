b3335db7130b9ad0bb62f6341b82eaf10eb650ad
cocoritz <67122306+cocoritz@users.noreply.github.com>
2021-12-23 14:32:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,4 @@ st.title('Sensing and IoT')
 #document.getElementById('app').innerHTML = `
 
 #<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=d315592c-260e-40a6-b67f-bb67e628d83d&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
-`
+

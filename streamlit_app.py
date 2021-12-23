e52775ec032a6821946aaf5aaef5099c601fd7c2
cocoritz <67122306+cocoritz@users.noreply.github.com>
2021-12-23 14:49:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,22 @@
 import streamlit as st
 import pymongo
 import pandas as pd
+import pymongo
 
 
+st. set_page_configurat(page_title='Survey Results')
 st.title('Sensing and IoT')
 tickers = ('#climatechange', '#energy','#energycrisis')
 dropdown= st.multiselect('Pick your #',tickers)
 
 start= st.date_input('Start',value= pd.to_datetime('2021-12-01'))
 end= st.date_input('End',value= pd.to_datetime('2021-12-20'))
+
+#if len(dropdown)>0:
+# df=pymongo.client.mydb
+#  items = db.mycollection.find(dropdown,start,end)
+#  items = list(items)  
+
                      
 
 #document.getElementById('app').innerHTML = `

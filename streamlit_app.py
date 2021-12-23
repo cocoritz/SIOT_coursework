933b4f6e6d93beca76306094a43653c929bbdf20
cocoritz <67122306+cocoritz@users.noreply.github.com>
2021-12-23 15:17:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,7 @@ start= st.date_input('Start',value= pd.to_datetime('2021-12-01'))
 end= st.date_input('End',value= pd.to_datetime('2021-12-20'))
 
 image='<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=d315592c-260e-40a6-b67f-bb67e628d83d&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>'
-components.iframe(image)
-
+st.components.v1.html(image, width=640, height=480, scrolling=False)
 
          
 

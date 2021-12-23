579914ea09bea20433a6a4a41c1727656a44f577
cocoritz <67122306+cocoritz@users.noreply.github.com>
2021-12-23 15:23:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import streamlit.components.v1 as components
 
 st.set_page_config(page_title='Survey Results')
 st.header('Sensing and IoT')
-st.subheader('Original Data')
+st.subheader('Energy')
 tickers = ('#climatechange', '#energy','#energycrisis')
 dropdown= st.multiselect('Pick your #',tickers)
 
@@ -19,10 +19,10 @@ end= st.date_input('End',value= pd.to_datetime('2021-12-20'))
 image='<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=d315592c-260e-40a6-b67f-bb67e628d83d&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>'
 st.components.v1.html(image, width=640, height=480, scrolling=False)
 
-         
+st.subheader('Tweets')
 
-st.subheader('Analysis and trend')
-st.subheader('Correlation')
+image='<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=0ca4fa3b-3ff8-49cb-bd6e-1ae9c3a620cf&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>'
+st.components.v1.html(image, width=640, height=480, scrolling=False)
 
 #if len(dropdown)>0:
 # df=pymongo.client.mydb

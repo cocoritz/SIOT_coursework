deea60ee4e8609dd067d916c621fe780377b125f
cocoritz <67122306+cocoritz@users.noreply.github.com>
2022-01-04 13:08:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ client = pymongo.MongoClient(st.secrets["mongo"])
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
 def get_data():
-    db = client.mydb
+    db = client.mydatabase
     items = db.mycollection.find()
     items = list(items)  # make hashable for st.cache
     return items

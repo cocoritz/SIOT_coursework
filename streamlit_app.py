7dce68f18258b5621afd88a440aa5e1f32139e00
cocoritz <67122306+cocoritz@users.noreply.github.com>
2022-01-04 12:22:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,26 @@ def main():
     page = st.sidebar.selectbox(
         "Select a Page",
         [
-            "Original data"
+            "Original data",
+            "Analysed data",
+            "Have a look"
+            
         ]
     )
 
+    #First Page
     if page == "Original data":
         homepage()
+
+    #Second Page
+    elif page == "Analysed data":
+        analyseddata()
+    
+    #Third Page
+    elif page == "Have a look":
+        information()
+
+  
 
 def homepage():
     st.write("""
@@ -39,17 +53,6 @@ def homepage():
     image2='<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=d24155ad-c4b4-4489-bbf7-efc169eb7a76&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>'
     chart2= st.components.v1.html(image2, width=640, height=480, scrolling=False)
 
-def main():
-    page = st.sidebar.selectbox(
-        "Select a Page",
-        [
-            "Analysed data"
-        ]
-    )
-
-    if page == "Analysed data":
-        analyseddata()
-        
        
 def analyseddata():
     
@@ -65,17 +68,7 @@ def analyseddata():
     df_tweets= pd.DataFrame(x_tweets)
     print(df)
 
-  
-def main():
-    page = st.sidebar.selectbox(
-        "Select a Page",
-        [
-            "Intresting information"
-        ]
-    )
-
-    if page == "Intresting information":
-        information()
+ 
         
 def information():
     class Tweet(object):
@@ -102,13 +95,6 @@ def information():
 if __name__ == "__main__":
     main()
 
-#if len(dropdown)>0:
-# df=pymongo.client.mydb
-#  items = db.mycollection.find(dropdown,start,end)
-#  items = list(items)  
-
-#chart1 = alt.Chart(df).mark_line().encode().properties(width=500, height=300)
-#chart2 = alt.Chart(df).mark_line().encode().properties(width=500, height=300)
 
 
                     

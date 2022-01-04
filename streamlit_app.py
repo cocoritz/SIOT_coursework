850c7bf76e70022330040ca092432cd7fd3a1940
cocoritz <67122306+cocoritz@users.noreply.github.com>
2022-01-04 20:11:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,28 +13,27 @@ import numpy as np
 
 DATA_URL = ('Tweets_climatechange_and_energy.csv')
 df_tweets = pd.read_csv(DATA_URL)
-st.write(df)
-st..plot()
 
-#df_tweets['create_at']=pd.to_datetime(df_tweets['create_at'])
-# df_tweets['create_at'] = df_tweets['create_at'].apply(remove_timezone)
 
-# #Keep relevant time
-# filt = ((df_tweets['create_at'] <= pd.to_datetime('2021-12-19 00:00'))& (df_tweets['create_at'] >= pd.to_datetime('2021-12-05 17:00')))
-# df_tweets=df_tweets.loc[filt]
 
-# #set time as index
-# df_tweets.set_index('create_at', inplace=True)
+df_tweets['create_at']=pd.to_datetime(df_tweets['create_at'])
+df_tweets['create_at'] = df_tweets['create_at'].apply(remove_timezone)
 
-# #Simplify calculation
-# df_tweets['number_of_tweets'] = 1
-# #df_tweets.drop('_id', axis = 1, inplace= True)
+# Keep relevant time
+filt = ((df_tweets['create_at'] <= pd.to_datetime('2021-12-19 00:00'))& (df_tweets['create_at'] >= pd.to_datetime('2021-12-05 17:00')))
+df_tweets=df_tweets.loc[filt]
 
-# #Resample per hour
-# df_tweets.drop('_id', axis = 1, inplace= True)
-# df_tweets = df_tweets['number_of_tweets'].resample('H').sum()
-# df_tweets.plot()
-# st.plot(df)
+# set time as index
+df_tweets.set_index('create_at', inplace=True)
+
+#Simplify calculation
+df_tweets['number_of_tweets'] = 1
+df_tweets.drop('_id', axis = 1, inplace= True)
+
+# Resample per hour
+df_tweets.drop('_id', axis = 1, inplace= True)
+df_tweets = df_tweets['number_of_tweets'].resample('H').sum()
+st.write(df_tweets)
 
 
 # Pull data from the collection.

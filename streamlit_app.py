f45ebfdd22f7f52dc529babd9d76c840eaa018df
cocoritz <67122306+cocoritz@users.noreply.github.com>
2022-01-04 19:56:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def load_data():
 df = load_data()
 
 # show data on streamlit
-st.write(df
+st.write(df)
 # # Initialize connection.
 # client = pymongo.MongoClient(st.secrets["mongo"])
 
@@ -149,10 +149,3 @@ st.write(df
 
 # if __name__ == "__main__":
 #     main()
-
-
-
-
-                    
-
-

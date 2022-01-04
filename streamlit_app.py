bcb0de683952c53349844786b1fcc2adfe0d67b2
cocoritz <67122306+cocoritz@users.noreply.github.com>
2022-01-04 12:03:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,37 @@ import pymongo
 import pandas as pd
 import pymongo
 import streamlit.components.v1 as components
+import matplotlib.pyplot as plt
+
+def main():
+    page = st.sidebar.selectbox(
+        "Select a Page",
+        [
+            "Original data"
+        ]
+    )
+
+    if page == "Original data":
+        homepage()
+
+def homepage():
+    st.write("""
+        # Sensing and Iot
+        ### original data ###
+        #""")
+    #tickers = ('#climatechange', '#energy','#energycrisis')
+    #dropdown= st.multiselect('Pick your #',tickers)
+
+    #start= st.date_input('Start',value= pd.to_datetime('2021-12-01'))
+    #end= st.date_input('End',value= pd.to_datetime('2021-12-20'))
+
+    image1='<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=d315592c-260e-40a6-b67f-bb67e628d83d&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>'
+    chart1= st.components.v1.html(image1, width=640, height=480, scrolling=False)
+
+    image2='<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=d24155ad-c4b4-4489-bbf7-efc169eb7a76&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>'
+    chart2= st.components.v1.html(image2, width=640, height=480, scrolling=False)
 
 
-st.set_page_config(page_title='Survey Results')
-st.header('Sensing and IoT')
-#st.subheader('Energy')
-#tickers = ('#climatechange', '#energy','#energycrisis')
-#dropdown= st.multiselect('Pick your #',tickers)
-
-#start= st.date_input('Start',value= pd.to_datetime('2021-12-01'))
-#end= st.date_input('End',value= pd.to_datetime('2021-12-20'))
-
-image1='<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=d315592c-260e-40a6-b67f-bb67e628d83d&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>'
-
-image2='<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=d24155ad-c4b4-4489-bbf7-efc169eb7a76&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>'
-chart2= st.components.v1.html(image2, width=640, height=480, scrolling=False)
 
 
 #if len(dropdown)>0:

dd44441ec427fae40f8055a1633e8f605b7e6f09
cocoritz <67122306+cocoritz@users.noreply.github.com>
2022-01-05 20:17:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ def homepage():
     chart1= st.components.v1.html(image1, width=640, height=480, scrolling=False)
     st.text("")
     st.text("")
+    st.text("")
     st.markdown('Second data stream')
     st.text('Amount of tweets containing climate change and energy words')             
     image2='<iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-twitter_api_project-zwapd/embed/charts?id=d24155ad-c4b4-4489-bbf7-efc169eb7a76&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>'
